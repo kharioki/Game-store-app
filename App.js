24ee22b80eb081c219e7d9ae647bb2345a0fc6ef
kharioki <antolisah@gmail.com>
2020-06-23 05:35:10 +0300

    app icons config

--- App.js
+++ App.js
@@ -1,18 +1,56 @@
 import React from 'react';
+import styled from 'styled-components';
+import Entypo from 'react-native-vector-icons/Entypo';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import HomeScreen from './components/screens/HomeScreen';
+import LiveScreen from './components/screens/LiveScreen';
+import ProfileScreen from './components/screens/ProfileScreen';
 
 const AppStack = createStackNavigator();
 const TabNav = createBottomTabNavigator();
 
+const tabBarOptions = {
+  showLabel: false,
+  style: {
+    backgroundColor: '#343434',
+    borderTopColor: '#343434',
+    paddingBottom: 12,
+  },
+};
+
+Entypo.loadFont();
+
 const TabNavScreen = () => {
   return (
-    <TabNav.Navigator>
+    <TabNav.Navigator
+      tabBarOptions={tabBarOptions}
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focussed}) => {
+          let iconName;
+
+          switch (route.name) {
+            case 'HomeScreen':
+              iconName = 'home';
+              break;
+
+            default:
+              break;
+          }
+
+          return (
+            <TabBarIconContainer focussed={focussed}>
+              <Entypo name={iconName} size={24} color="#ffffff" />
+            </TabBarIconContainer>
+          );
+        },
+      })}>
       <TabNav.Screen name="HomeScreen" component={HomeScreen} />
+      <TabNav.Screen name="LiveScreen" component={LiveScreen} />
+      <TabNav.Screen name="ProfileScreen" component={ProfileScreen} />
     </TabNav.Navigator>
   );
 };
@@ -20,7 +58,7 @@ const TabNavScreen = () => {
 const App = () => {
   return (
     <NavigationContainer>
-      <AppStack.Navigator>
+      <AppStack.Navigator headerMode="none">
         <AppStack.Screen name="App" component={TabNavScreen} />
       </AppStack.Navigator>
     </NavigationContainer>
@@ -28,3 +66,5 @@ const App = () => {
 };
 
 export default App;
+
+const TabBarIconContainer = styled.View``;
